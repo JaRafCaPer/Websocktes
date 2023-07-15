2d44b3627a9685beff11a23e49d3d86cc5e8de6e
Javier Caballero <caballeroperezjavier@gmail.com>
2023-07-15 18:13:50 -0500

    Se pulen detalles y se verifica que pase el testing

--- src/app.js
+++ src/app.js
@@ -1,35 +1,40 @@
-import express from 'express'
-import handlebars from 'express-handlebars'
-import { Server } from 'socket.io'
-import productRouter from './routes/product.router.js'
-import cartRouter from './routes/cart.router.js'
-import viewsRouter from './routes/views.router.js'
-import ProductManager from './manager/product.manager.js'
-import __dirname from './utils.js'
+import express from 'express';
+import handlebars from 'express-handlebars';
+import http from 'http';
+import { Server } from 'socket.io';
+import productRouter from './routes/product.router.js';
+import cartRouter from './routes/cart.router.js';
+import viewsRouter from './routes/views.router.js';
+import ProductManager from './manager/product.manager.js';
+import __dirname from './utils.js';
 
-const app = express()
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.engine('handlebars', handlebars.engine())
-app.set('views', __dirname + '/views')
-app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
 
-app.use('/', viewsRouter)
-app.use('/api/products', productRouter)
-app.use('/api/carts', cartRouter)
+app.use('/', viewsRouter);
+app.use('/api/products', productRouter);
+app.use('/api/carts', cartRouter);
 
-const httpServer = app.listen(8080)
-const io = new Server(httpServer)
+const port = 8080;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
 
-io.on('connection', socket => {
-    socket.on('new-product', async data => {
-        const productManager = new ProductManager()
-        await productManager.create(data)
-
-        const products = await productManager.list()
-        io.emit('reload-table', products)
-        
-    })
-})
+io.on('connection', (socket) => {
+    socket.emit('connected');
+  socket.on('new-product', async (data) => {
+    const productManager = new ProductManager();
+    await productManager.create(data);
+    const products = await productManager.list();
+    io.emit('reload-table', products);
+    
+  });
+});
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,28 +1,29 @@
-import { Router } from "express";
-import ProductManager from '../manager/product.manager.js'
+import { Router } from 'express';
+import ProductManager from '../manager/product.manager.js';
 
-const router = Router()
-const productManager = new ProductManager()
+const router = Router();
+const productManager = new ProductManager();
 
 router.get('/', async (req, res) => {
-    const products = await productManager.list()
-    res.render('products', { products })
-})
+  const products = await productManager.list();
+  res.render('products', { products });
+});
 
-router.get('/products-realtime', async (req, res) => {
-    const products = await productManager.list()
-    res.render('products_realtime', { products })
-})
+router.get('/realtimeproducts', async (req, res) => {
+  const products = await productManager.list();
+  res.render('products_realtime', { products });
+});
 
 router.get('/form-products', async (req, res) => {
-    res.render('form', {})
-})
+  res.render('form', {});
+});
 
 router.post('/form-products', async (req, res) => {
-    const data = req.body
-    const result = await productManager.create(data)
+  const data = req.body;
+  await productManager.create(data);
+  const products = await productManager.list();
+  io.emit('reload-table', products);
+  res.redirect('/');
+});
 
-    res.redirect('/')
-})
-
-export default router
+export default router;
